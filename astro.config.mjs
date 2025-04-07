f1003a8e6d03c93418895adefcaabe278206cc75
yanghua <385362330@qq.com>
2025-04-07 13:28:35 +0800

    build: 添加github部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,10 @@ import Unocss from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  site:"https://gnayauh98.github.io",
   integrations: [
     Unocss({
       injectReset: true
     })
   ],
-  output: 'server'
 });
---
+++ posts/plan.md
@@ -0,0 +1,11 @@
+---
+id: personal-plan
+title: 个人计划
+status: private
+---
+
+## 软件开发计划
+
+- [ ] 基于excalidraw开发一个绘板笔记软件
+
+
